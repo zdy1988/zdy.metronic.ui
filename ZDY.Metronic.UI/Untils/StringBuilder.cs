7793ba80d17918848d715210aea3f626cdca5fea
zhang hua <virus_zhh@126.com>
2020-04-24 18:13:03 +0800

    11

---
+++ ZDY.Metronic.UI/Untils/StringBuilder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace ZDY.Metronic.UI
+{
+    internal static class StringBuilder
+    {
+        internal static string Build(params ValueTuple<string, bool>[] inputs)
+        {
+            var strings = inputs.Where(t => t.Item2 == true);
+
+            return strings.Count() > 0 ? String.Join(",", strings) : "";
+        }
+    }
+}
